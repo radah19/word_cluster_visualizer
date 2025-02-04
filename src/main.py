eedd8953a0d39cee24fbe6910015104d388914c4
Anuj Adulla <anuj.adulla@gmail.com>
2025-02-04 18:06:46 -0500

    Added Spell Checker to support Stemming

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ import pandas as pd
 from nltk.stem import LancasterStemmer
 from pyvis.network import Network
 import random
+from spellchecker import SpellChecker
+
+def main():
+    visualizeWordFreqData()
 
 # Vars ----------------------------------------------------------------------------------------------
 # vocab = pd.read_pickle('./data/vocab')
@@ -12,7 +16,6 @@ doc_lt = pd.read_pickle('./data/temp.pkl')
 
 # Word Freq Visualization ---------------------------------------------------------------------------
 def visualizeWordFreqData():
-
     # Simple Counter for Words
     word_freqs = {}
 
@@ -22,18 +25,20 @@ def visualizeWordFreqData():
                 word_freqs[word] = 0
             word_freqs[word] += 1
 
-    # Display Frequencies
+    # Display Frequencies on PyVis HTML Graph
     net = Network()
     net.toggle_physics(True)
     
     colorcode_dict = {}
     lancaster = LancasterStemmer()
+    spell = SpellChecker()
 
     for word, word_freq in word_freqs.items():
         if word == None:
             continue
 
-        stemmed_word = lancaster.stem(word)
+        corrected_word = spell.correction(word)
+        stemmed_word = lancaster.stem(corrected_word if corrected_word != None else word)
 
         if stemmed_word not in colorcode_dict:
             colorcode_dict[stemmed_word] = "%06x" % random.randint(0, 0xFFFFFF)
@@ -47,4 +52,4 @@ def visualizeWordFreqData():
 
     net.save_graph('word_freq_diagram.html')
 
-visualizeWordFreqData()
+main()
